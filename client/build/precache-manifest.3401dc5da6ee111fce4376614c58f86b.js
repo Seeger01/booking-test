3200776f41f64dbbdecfd4335c42f4c3a4dcbffa
Seeger01 <cph-ps183@cphbusiness.dk>
2019-05-10 14:50:51 +0200

    heyy

---
+++ client/build/precache-manifest.3401dc5da6ee111fce4376614c58f86b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a3eeab0fa8ff9fa4eee1ffb360e8e35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1790a0a24568f0bc9b4",
+    "url": "/static/css/main.2251ef14.chunk.css"
+  },
+  {
+    "revision": "31f9bd6f2f208c570d07",
+    "url": "/static/js/2.e3a084ee.chunk.js"
+  },
+  {
+    "revision": "e1790a0a24568f0bc9b4",
+    "url": "/static/js/main.1d2d764d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
